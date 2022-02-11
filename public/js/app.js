1a23f8df4b8136e0d4f94034dd1a61ba796b1235
Manoj Srinivas <manojsrinivas@Manojs-MacBook-Pro.local>
2022-02-10 19:37:12 -0600

    re-added client side JS reference in index.hbs

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,12 @@ const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
 const message2 = document.querySelector('#message-2')
 
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    const location = search.value
     message1.textContent = 'Loading...'
     message2.textContent = ''
-    fetch(`/weather?address=${search.value}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             message1.textContent = 'Error: ' + data.error
